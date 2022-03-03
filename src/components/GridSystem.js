327b116a8bfdc11e332d92f990b71a7dcb969cb1
kanakabhashitha <kanakabhashitha@gmail.com>
2022-03-03 11:03:42 +0530

    change grid

--- src/components/GridSystem.js
+++ src/components/GridSystem.js
@@ -7,32 +7,33 @@ function GridSystem(props) {
   return (
     <>
       <div className="gridTitle">Grid System</div>
-      <Container>
-        <Row md={2} className="firstRow  justify-content-md-center">
-          <Col md={3} className="A">
+
+      <Container className="mainContainer">
+        <Row md={2} className="row-1">
+          <Col xs={12} md={6} className="row-1-col-1">
             A
           </Col>
-          <Col md={3} className="B">
+          <Col xs={12} md={6} className="row-1-col-2">
             B
           </Col>
         </Row>
 
-        <Row md={2} className="secondRow  justify-content-md-center ">
-          <Col md={2} className="bg-warning">
-            <Row className="C">C</Row>
-            <Row className="D">D</Row>
+        <Row md={2} className="row-2">
+          <Col xs={12} md={4} className="row-2-col-1">
+            <Row className="row-2-col-1-col-1">C</Row>
+            <Row className="row-2-col-1-col-2">E</Row>
           </Col>
 
-          <Col md={4} className="thirdcol">
-            <Row md={1} className="thirdRow">
-              E
+          <Col xs={12} md={8} className="row-2-col-2">
+            <Row md={1} className="row-2-col-2-row-1">
+              D
             </Row>
 
-            <Row row={2} className="forthRow">
-              <Col md={6} className="F">
+            <Row xs={12} md={2} className="row-2-col-2-row-2">
+              <Col xs={6} md={6} className="row-2-col-2-row-2-col-1">
                 F
               </Col>
-              <Col md={6} className="G">
+              <Col xs={6} md={6} className="row-2-col-2-row-2-col-2">
                 G
               </Col>
             </Row>
